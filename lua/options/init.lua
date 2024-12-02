1a8e3967b05626409b814bbaf0d7bf94b07a0c15
matthis-k <matthis.kaelble@gmail.com>
2024-12-02 15:50:41 +0100

    add on_yank

--- lua/options/init.lua
+++ lua/options/init.lua
@@ -62,5 +62,12 @@ if vim.fn.has("nvim-0.9.0") == 1 then
     opt.shortmess:append({ C = true })
 end
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    callback = function ()
+        vim.highlight.on_yank({ higroup = "Visual" })
+    end,
+})
+
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
