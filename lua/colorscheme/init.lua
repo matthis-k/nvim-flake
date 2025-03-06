8a6d300b9ebacad492ffb3b1a71852340385a99d
matthis-k <matthis.kaelble@gmail.com>
2025-03-06 06:39:21 +0100

    make the autocomplete selection great again!!!1!!

--- lua/colorscheme/init.lua
+++ lua/colorscheme/init.lua
@@ -1,1 +1,9 @@
-require("base16-colorscheme").setup("catppuccin-mocha",{ telescope_borders = true })
+local base16 = require("base16-colorscheme")
+base16.setup("catppuccin-mocha", { telescope_borders = true })
+
+local colors = base16.colors
+local hl = require("base16-colorscheme").highlight
+
+-- for blink.cmp to look clean
+hl.PMenuSel = { guifg = nil, guibg = colors.base02, gui = nil, guisp = nil, ctermfg = nil, ctermbg = colors.cterm02 }
+hl.CmpItemAbbr = { guifg = nil, guibg = nil, gui = nil, guisp = nil, ctermfg = nil, ctermbg = nil }
